807828249640470e96881cdfc5001ddef126156b
Yosef Saputra <yosaputr@cisco.com>
2023-09-22 10:45:17 -0500

    fix: add debug usage in main.go

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/yosefsaputra/scratchgo/debug"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+
+	// debug usage
+	mystr := "test"
+	myint := 100
+	my := map[string]any{"int": myint, "str": mystr}
+	debug.PtA(my)
+	debug.Pt("mystr", mystr)
+	debug.Pt("my", my)
 }
